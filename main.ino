229e7b591582d727ff8947429fd8a592470f05b0
Seacoast Makers <seacoastmakers@gmail.com>
2021-02-05 08:52:36 -0500

    Update main.ino
    Working button and display. No long-term memory yet.

--- main.ino
+++ main.ino
@@ -2,31 +2,60 @@
 /*Hardware:
 -Arduino Uno
 -Arcade button
--LCD Panel
+-Phico Datavision LCD-display module model no: DV-16215-1-S2RB
 -
+
 */
 
-#include <EEPROM.h>
-int addr = 0; //EEPROM address
+#include <DV16215.h>
+//#include String
 
+// Create SoftwareSerial
+SoftwareSerial connection(10, 11, true); // RX, TX, inverted logic
 
-void setup(){
-  
-  
+// Create myLCD with SoftwareSerial connection
+LCD_DV16215 myLCD(&connection);
+
+const int BUTTON = 8;
+const int delaytime = 50;
+int currentcount= 0;
+char countline[16];
+
+void setup() 
+{
+  Serial.begin(9600,SERIAL_8N1); // debugging if needed
+  myLCD.clearAllDisplays();   // Clear everything just in case
+  pinMode(BUTTON, INPUT);
+  myLCD.clearDisplay();
+  msgDisp1();
 }
 
-
-void loop(){
-  
-  
-  
-}
-
-
-void incrementCounter(){
-  EEPROM.write(addr, val);
-  addr = addr + 1;
-  if (addr == EEPROM.length()) {
-    addr = 0;
+void loop() 
+{
+  if(digitalRead(BUTTON) == HIGH){
+    currentcount++;
+    myLCD.clearDisplay();
+    msgDisp1();
+    delay(200);
   }
+}
+
+void msgDisp1()
+{
+  //myLCD.clearDisplay();
+  myLCD.setDisplayTop(0,1);
+  myLCD.write("Pun Count:");
+  myLCD.setDisplayTop(0,2);
+  char cstr[16];
+  itoa(currentcount, cstr, 10);
+  myLCD.write(cstr);
+}
+
+void msgDisp2()
+{
+  myLCD.setDisplayBottom(0,1);
+  myLCD.write("_____________");
+  myLCD.setDisplayBottom(0,2);
+  myLCD.write("_____________");
+  delay(2000);
 }
