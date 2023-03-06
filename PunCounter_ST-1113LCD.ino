4d906d5058e752c45589b6e500ea7c8936a6b73e
Seacoast Makers <seacoastmakers@gmail.com>
2023-03-06 17:27:26 -0500

    Update PunCounter_ST-1113LCD.ino
    Re-organized everything, added startup music, added comments

--- PunCounter_ST-1113LCD.ino
+++ PunCounter_ST-1113LCD.ino
@@ -1,15 +1,24 @@
 /*
+PunCounter - a simple device to incenivize telling puns. 
+Displays a count on the screen. Every time the button is pressed, a sound plays and the count is updated.
+Great if you're a real button-pusher!
+Written by Alex Nunn 2023 March 6th
+
+Hardware:
 Arduino 2x16 LCD 1602A
 Arcade button
+2k ohm pulldown resistor between button pin and ground
 Speaker
-PunCounter - a simple button-press counter that stores the value inbetween power cycles
-Written by Alex Nunn
+
 */
 
 #include <LiquidCrystal.h>
 #include <EEPROM.h>
 
-//LCD pin to Arduino
+const int BUTTON = A1;
+const int SPEAKER = A2;
+
+//1602A LCD pins
 const int pin_BL = 10; 
 const int pin_EN = 9; 
 const int pin_RS = 8; 
@@ -18,27 +27,33 @@ const int pin_d6 = 6;
 const int pin_d5 = 5; 
 const int pin_d4 = 4; 
 
+//Create lcd object
 LiquidCrystal lcd( pin_RS,  pin_EN,  pin_d4,  pin_d5,  pin_d6,  pin_d7);
 
-//Address for EEPROM storage
+//EEPROM address for storing the count
 int EEPROMaddr = 0;
 
-//Pin info for button
-const int BUTTON = A1;
+//Variable for managing the count while powered on
+unsigned int currentcount= 0;
 
-//Speaker and sound settings
-const int SPEAKER = 3;
-//Notes to play. Obtained from toneMelody example file.
-int melody[] = {
+//Startup music
+int melody1[]={
+  659,784,880
+};
+
+int noteDurations1[] = {
+  8, 16, 4
+};
+
+//Button sound effect
+int melody2[] = {
   784, 587, 392
 };
-int noteDurations[] = {
-  16, 16, 16
+int noteDurations2[] = {
+  8, 16, 8
 };
 
 
-const int delaytime = 50;
-unsigned int currentcount= 0;
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -47,24 +62,30 @@ void setup() {
   pinMode(BUTTON, INPUT);
   
   lcd.begin(16, 2);
+
+  //Display a startup screen
   lcd.setCursor(0,0);
   lcd.print("Hello, Jokester");
   lcd.setCursor(0,1);
   lcd.print("LOLOLOLOLOL");
   
   //EEPROM.put(EEPROMaddr, 0); //To reset to 0 or clear any random data, run this one time. Recomment when done.
-  EEPROM.get(EEPROMaddr, currentcount);
-  delay(100);
-  msgDisp1();
-  playTone();
+
+  
+  EEPROM.get(EEPROMaddr, currentcount); //Load count from EEPROM
+  delay(100); //Wait to avoid issues with EEPROM
+  writeDisplayMain(); //Display main screen
+  playTone(melody1,noteDurations1, 3); //Play startup music
 }
 
+//Main loop. When the button is pressed, the count is incremented by one and written to the EEPROM,
+//the numerical part of the screen is updated to show the new count, and a sound plays
 void loop() {
  if(digitalRead(BUTTON) == HIGH){
     currentcount++;
     EEPROM.put(EEPROMaddr, currentcount);
-    msgUpdate();
-    playTone();
+    writeDisplayCount();
+    playTone(melody2,noteDurations2, 3);
     delay(150);
   }
 }
@@ -72,7 +93,8 @@ void loop() {
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-void msgDisp1()
+//Main display screen
+void writeDisplayMain()
 {
   lcd.clear();
   lcd.setCursor(0,0);
@@ -83,7 +105,8 @@ void msgDisp1()
   lcd.write(cstr);
 }
 
-void msgUpdate()
+//Update the section of the screen with the count
+void writeDisplayCount()
 {
   lcd.setCursor(0,1);
   char cstr[16];
@@ -91,12 +114,14 @@ void msgUpdate()
   lcd.write(cstr);
 }
 
-void playTone()
+//Plays melodies saved in an integer array format
+void playTone(int melody[], int noteDurations[], int melodyLength)
 {
+  //Display LOL in the bottom right corner of the screen
   lcd.setCursor(13,1);
   lcd.write("LOL");
   
-  for (int thisNote = 0; thisNote < 3; thisNote++) {
+  for (int thisNote = 0; thisNote < melodyLength; thisNote++) {
     // note duration = one second divided by the note type.
     //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
     int noteDuration = 1000 / noteDurations[thisNote];
@@ -107,6 +132,8 @@ void playTone()
     delay(pauseBetweenNotes);
     noTone(SPEAKER);
   }
+
+  //Clear the LOL from the corner of the screen
   lcd.setCursor(13,1);
   lcd.write("   ");
 }
